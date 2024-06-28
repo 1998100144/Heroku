10a92d64fc49a4b13ca72a6f1be1ca0a23ba54d3
Kasimova Zarina <zosyas14@gmail.com>
2024-06-28 20:55:22 +0400

    New test add

---
+++ src/test/java/tests/TyposTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TyposTest extends BaseTest {
+
+    @Test
+    public void typosTest() {
+        typosPage.open();
+        assertEquals(typosPage.findTypos(), " Sometimes you'll see a typo, other times you won't.");
+
+    }
+}
